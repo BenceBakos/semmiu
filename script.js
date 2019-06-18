4553944b992e9594f3030b0ba8487d7ca7c33b0b
Baboskence <ceben.bb@gmail.com>
2019-06-18 13:37:28 +0200

    .

---
+++ script.js
@@ -0,0 +1,26 @@
+(function () {
+	/*****************FEED********************/
+	const feed = document.getElementById('feed');
+	function postElement(title, date, content, hash) {}
+	function lastPost() {
+		return feed.children[[...feed.children].length - 1];
+	}
+	function firstPost() {
+		return feed.children[0];
+	}
+	//on Load
+	fetch('postList.txt').then(function (res) {
+		//error handling
+		if (res.status !== 200) {
+			console.log('ERROR:' + res.status);
+			return;
+		}
+		console.log(res);
+	})
+
+	//on click new
+
+	//on click olders
+
+}
+	());
